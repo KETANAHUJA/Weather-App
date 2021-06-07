945ee2452a68bf887ab869fc4287d3b59a23dddd
Ketan Ahuja <56186989+KETANAHUJA@users.noreply.github.com>
2021-06-07 16:31:36 +0530

    firstCommit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    
+    res.sendFile(__dirname+"/index.html");
+});
+app.post("/",(req,res)=>{
+
+    const query = req.body.cityName;
+    const apikey = "d4d79eafc70d951d9720e023f404df38";
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid="+apikey+"&q="+query+"&units=metric";
+    https.get(url,(response)=>{
+        console.log(response.statusCode);
+    response.on("data",(data)=>{
+        const weatherdata = JSON.parse(data);
+        // console.log(weatherdata);
+        const temp = weatherdata.main.temp;
+        // console.log(temp);
+        const des = weatherdata.weather[0].description;
+        const city = weatherdata.name;
+        const icon = weatherdata.weather[0].icon;
+        const imageUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+
+        // console.log(des);
+        res.write(`<p>The weather is Currently : ${des}</p>`);
+        res.write(`<h1>The Temprature in ${city} is :  ${temp} Celcius</h1>`);
+        res.write("<img src="+imageUrl+"></img>")
+        res.send();
+    })
+
+    });
+
+})
+
+
+
+
+app.listen(2500,()=>{
+console.log("server Started");
+});
